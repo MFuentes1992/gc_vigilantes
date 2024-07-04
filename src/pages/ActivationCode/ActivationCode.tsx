e3025513174f8da633f3fddc724546bb3be8a1e2
MFuentes1992 <markfuentes2991@gmail.com>
2024-07-03 18:16:31 -0600

    Change credentials

--- src/pages/ActivationCode/ActivationCode.tsx
+++ src/pages/ActivationCode/ActivationCode.tsx
@@ -3,12 +3,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { View, Text, TextInput, TouchableOpacity, Alert } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import {
-	authenticate,
-	container,
-	container_animation,
-	getActivationCode,
-	submit_button,
-	title_container,
+  authenticate,
+  container,
+  container_animation,
+  getActivationCode,
+  submit_button,
+  title_container,
 } from "./constants";
 import { RootState } from "@gcVigilantes/store";
 import { ROUTES, getLabelApp } from "@gcVigilantes/utils";
@@ -24,250 +24,250 @@ import { ALERT_TYPES } from "@gcVigilantes/Components/Alerts/constants";
 import LottieView from "lottie-react-native";
 
 export const ActivationCode = ({ navigation }: any) => {
-	const dispatch = useDispatch();
-	const [activationCode, setActivationCode] = useState<string>("");
-	const [dataBaseCode, setDataBaseCod] = useState<string>("");
-	const { access_token } = useSelector((state: RootState) => state.userData);
-	const [loading, setLoading] = useState<boolean>(true);
-	const preferences = useSelector((state: RootState) => state.preferences);
+  const dispatch = useDispatch();
+  const [activationCode, setActivationCode] = useState<string>("");
+  const [dataBaseCode, setDataBaseCod] = useState<string>("");
+  const { access_token } = useSelector((state: RootState) => state.userData);
+  const [loading, setLoading] = useState<boolean>(true);
+  const preferences = useSelector((state: RootState) => state.preferences);
 
-	useEffect(() => {
-		setLoading(true);
-		AsyncStorage.getItem("database_code")
-			.then((dbCode) => {
-				if (dbCode === "" || dbCode === null) {
-					setLoading(false);
-				}
-				if (dbCode) {
-					setDataBaseCod(dbCode);
-					AsyncStorage.getItem("token_instalacion")
-						.then((res) => {
-							if (res) {
-								Authenticate(dbCode).then((res) => {
-									if (res) {
-										AsyncStorage.getItem("activation_code")
-											.then((res) => {
-												if (res) {
-													getActivationCode(res)
-														.then((res) => res.json())
-														.then((data) => {
-															if (
-																data.code === 203 ||
-																data.code === "203" ||
-																data.code === 200 ||
-																data.code === "200"
-															) {
-																navigation.replace(ROUTES.HOME);
-															} else {
-																setLoading(false);
-															}
-														})
-														.catch((error) => {
-															setLoading(false);
-															console.log(
-																"Error al obtener token de activación",
-																error
-															);
-														});
-												}
-											})
-											.catch((error) => {
-												setLoading(false);
-												console.log(
-													"Error al obtener token de activación",
-													error
-												);
-											});
-									}
-								});
-							} else {
-								setLoading(false);
-							}
-						})
-						.catch((error) => {
-							setLoading(false);
-							console.log("Error al obtener token de instalación", error);
-						});
-				}
-			})
-			.catch((error) => {
-				setLoading(false);
-				console.log("Error al obtener database_code", error);
-			});
-	}, []);
+  useEffect(() => {
+    setLoading(true);
+    AsyncStorage.getItem("database_code")
+      .then((dbCode) => {
+        if (dbCode === "" || dbCode === null) {
+          setLoading(false);
+        }
+        if (dbCode) {
+          setDataBaseCod(dbCode);
+          AsyncStorage.getItem("token_instalacion")
+            .then((res) => {
+              if (res) {
+                Authenticate(dbCode).then((res) => {
+                  if (res) {
+                    AsyncStorage.getItem("activation_code")
+                      .then((res) => {
+                        if (res) {
+                          getActivationCode(res)
+                            .then((res) => res.json())
+                            .then((data) => {
+                              if (
+                                data.code === 203 ||
+                                data.code === "203" ||
+                                data.code === 200 ||
+                                data.code === "200"
+                              ) {
+                                navigation.replace(ROUTES.HOME);
+                              } else {
+                                setLoading(false);
+                              }
+                            })
+                            .catch((error) => {
+                              setLoading(false);
+                              console.log(
+                                "Error al obtener token de activación",
+                                error
+                              );
+                            });
+                        }
+                      })
+                      .catch((error) => {
+                        setLoading(false);
+                        console.log(
+                          "Error al obtener token de activación",
+                          error
+                        );
+                      });
+                  }
+                });
+              } else {
+                setLoading(false);
+              }
+            })
+            .catch((error) => {
+              setLoading(false);
+              console.log("Error al obtener token de instalación", error);
+            });
+        }
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.log("Error al obtener database_code", error);
+      });
+  }, []);
 
-	const Authenticate = async (dbCode: string) => {
-		const res = await InitializeConnection(
-			"analisis@dasgalu.com.mx",
-			"123456",
-			dbCode,
-			authenticate
-		);
-		console.log("Authenticate Fetch", res);
-		if (res) {
-			dispatch(
-				setUserData({
-					access_token: res.access_token,
-					database_code: dbCode,
-					name: res.name,
-					residence: res.residence,
-					id: res.id,
-				})
-			);
-			return true;
-		}
-		return false;
-	};
+  const Authenticate = async (dbCode: string) => {
+    const res = await InitializeConnection(
+      "analisis@dasgalu.com.mx",
+      "analisis123",
+      dbCode,
+      authenticate
+    );
+    console.log("Authenticate Fetch", res);
+    if (res) {
+      dispatch(
+        setUserData({
+          access_token: res.access_token,
+          database_code: dbCode,
+          name: res.name,
+          residence: res.residence,
+          id: res.id,
+        })
+      );
+      return true;
+    }
+    return false;
+  };
 
-	const handleTextChange = (value: string) => {
-		setActivationCode(value);
-	};
+  const handleTextChange = (value: string) => {
+    setActivationCode(value);
+  };
 
-	const handleDataBaseChange = (value: string) => {
-		setDataBaseCod(value);
-	};
+  const handleDataBaseChange = (value: string) => {
+    setDataBaseCod(value);
+  };
 
-	const handleSubmit = () => {
-		if (dataBaseCode === "") {
-			setLoading(false);
-			Alert.alert(
-				"Error",
-				getLabelApp(preferences.language, "app_empty_field")
-			);
-			return;
-		}
-		if (activationCode === "") {
-			setLoading(false);
-			Alert.alert(
-				"Error",
-				getLabelApp(preferences.language, "app_empty_field")
-			);
-			return;
-		}
-		Authenticate(dataBaseCode)
-			.then((raw) => {
-				if (raw) {
-					getActivationCode(activationCode)
-						.then((res) => {
-							res
-								.json()
-								.then((data) => {
-									const resp = JSON.parse(JSON.stringify(data));
-									if (resp.code === 200 || resp.code === "200") {
-										AsyncStorage.setItem(
-											"token_instalacion",
-											data.token_instalacion
-										)
-											.then(() => {
-												AsyncStorage.setItem("activation_code", activationCode);
-												AsyncStorage.setItem("database_code", dataBaseCode);
-												navigation.replace(ROUTES.HOME);
-											})
-											.catch((error) => {
-												dispatch(
-													setShowAlert({
-														showAlert: true,
-														title: "Error",
-														message: error,
-														type: ALERT_TYPES.ERROR,
-													})
-												);
-											});
-									} else {
-										dispatch(
-											setShowAlert({
-												showAlert: true,
-												title: "Error",
-												message: resp.message,
-												type: ALERT_TYPES.ERROR,
-											})
-										);
-									}
-									return resp;
-								})
-								.catch((error) =>
-									console.log("Error al parsear token de activación", error)
-								);
-						})
-						.catch((error) =>
-							console.log("Error al obtener token de activación", error)
-						);
-				}
-			})
-			.catch((error) => {
-				setLoading(false);
-				console.log("Error al Iniciar conexón", error);
-			})
-			.finally(() => setLoading(false));
-	};
+  const handleSubmit = () => {
+    if (dataBaseCode === "") {
+      setLoading(false);
+      Alert.alert(
+        "Error",
+        getLabelApp(preferences.language, "app_empty_field")
+      );
+      return;
+    }
+    if (activationCode === "") {
+      setLoading(false);
+      Alert.alert(
+        "Error",
+        getLabelApp(preferences.language, "app_empty_field")
+      );
+      return;
+    }
+    Authenticate(dataBaseCode)
+      .then((raw) => {
+        if (raw) {
+          getActivationCode(activationCode)
+            .then((res) => {
+              res
+                .json()
+                .then((data) => {
+                  const resp = JSON.parse(JSON.stringify(data));
+                  if (resp.code === 200 || resp.code === "200") {
+                    AsyncStorage.setItem(
+                      "token_instalacion",
+                      data.token_instalacion
+                    )
+                      .then(() => {
+                        AsyncStorage.setItem("activation_code", activationCode);
+                        AsyncStorage.setItem("database_code", dataBaseCode);
+                        navigation.replace(ROUTES.HOME);
+                      })
+                      .catch((error) => {
+                        dispatch(
+                          setShowAlert({
+                            showAlert: true,
+                            title: "Error",
+                            message: error,
+                            type: ALERT_TYPES.ERROR,
+                          })
+                        );
+                      });
+                  } else {
+                    dispatch(
+                      setShowAlert({
+                        showAlert: true,
+                        title: "Error",
+                        message: resp.message,
+                        type: ALERT_TYPES.ERROR,
+                      })
+                    );
+                  }
+                  return resp;
+                })
+                .catch((error) =>
+                  console.log("Error al parsear token de activación", error)
+                );
+            })
+            .catch((error) =>
+              console.log("Error al obtener token de activación", error)
+            );
+        }
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.log("Error al Iniciar conexón", error);
+      })
+      .finally(() => setLoading(false));
+  };
 
-	return (
-		<ScrollView>
-			{!loading && (
-				<View style={container}>
-					<View style={title_container}>
-						<Text style={app_text_title}>
-							{getLabelApp(preferences.language, "app_activation_code")}
-						</Text>
-					</View>
-					<View style={card_styles}>
-						<TextInput
-							style={{
-								fontSize: 16,
-								height: 40,
-								padding: 10,
-							}}
-							value={activationCode}
-							onChangeText={handleTextChange}
-							placeholder={getLabelApp(
-								preferences.language,
-								"app_activation_code_placeholder"
-							)}
-						/>
-					</View>
-					<View style={card_styles}>
-						<TextInput
-							style={{
-								fontSize: 16,
-								height: 40,
-								padding: 10,
-							}}
-							value={dataBaseCode}
-							onChangeText={handleDataBaseChange}
-							placeholder={getLabelApp(
-								preferences.language,
-								"app_database_code"
-							)}
-						/>
-					</View>
-					<View style={card_styles}>
-						<TouchableOpacity style={submit_button} onPress={handleSubmit}>
-							<Text style={{ color: app_colors.white }}>
-								{getLabelApp(preferences.language, "app_submit_continue")}
-							</Text>
-						</TouchableOpacity>
-					</View>
-				</View>
-			)}
-			{loading && (
-				<View style={container_animation}>
-					<Text>
-						{getLabelApp(
-							preferences.language,
-							"app_activation_code_initialize"
-						)}
-					</Text>
-					<LottieView
-						source={require("../../../assets/initialize.json")}
-						style={{
-							width: 200,
-							height: 200,
-						}}
-						autoPlay
-						loop
-					/>
-				</View>
-			)}
-		</ScrollView>
-	);
+  return (
+    <ScrollView>
+      {!loading && (
+        <View style={container}>
+          <View style={title_container}>
+            <Text style={app_text_title}>
+              {getLabelApp(preferences.language, "app_activation_code")}
+            </Text>
+          </View>
+          <View style={card_styles}>
+            <TextInput
+              style={{
+                fontSize: 16,
+                height: 40,
+                padding: 10,
+              }}
+              value={activationCode}
+              onChangeText={handleTextChange}
+              placeholder={getLabelApp(
+                preferences.language,
+                "app_activation_code_placeholder"
+              )}
+            />
+          </View>
+          <View style={card_styles}>
+            <TextInput
+              style={{
+                fontSize: 16,
+                height: 40,
+                padding: 10,
+              }}
+              value={dataBaseCode}
+              onChangeText={handleDataBaseChange}
+              placeholder={getLabelApp(
+                preferences.language,
+                "app_database_code"
+              )}
+            />
+          </View>
+          <View style={card_styles}>
+            <TouchableOpacity style={submit_button} onPress={handleSubmit}>
+              <Text style={{ color: app_colors.white }}>
+                {getLabelApp(preferences.language, "app_submit_continue")}
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      )}
+      {loading && (
+        <View style={container_animation}>
+          <Text>
+            {getLabelApp(
+              preferences.language,
+              "app_activation_code_initialize"
+            )}
+          </Text>
+          <LottieView
+            source={require("../../../assets/initialize.json")}
+            style={{
+              width: 200,
+              height: 200,
+            }}
+            autoPlay
+            loop
+          />
+        </View>
+      )}
+    </ScrollView>
+  );
 };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,80 +1,80 @@
 import lang_es from "@gcVigilantes/utils/Messages/lang_esp.json";
 
 export const ENDPOINTS = {
-	BASE_URL: "https://apimovilgc.dasgalu.net",
-	QR: "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=",
-	CATALOG_TIPO_VISITAS: "/visita/catalogs/GetTipoVisita/index.php",
-	CATALOG_TIPO_INGRESO: "/visita/catalogs/GetTipoIngreso/index.php",
-	VISITAS: {
-		BY_UNIQUEID: "/visita/read-qr/index.php?qr={qr}",
-		VEHICLES: "/visita/getVisitVehicles/index.php?qr={qr}",
-		UPDATE: "/visita/update/index.php",
-	},
-	VIGILANTE: {
-		CODE: "/vigilante/activationCode/index.php",
-	},
+  BASE_URL: "https://movil.gestioncomunidad.mx",
+  QR: "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=",
+  CATALOG_TIPO_VISITAS: "/visita/catalogs/GetTipoVisita/index.php",
+  CATALOG_TIPO_INGRESO: "/visita/catalogs/GetTipoIngreso/index.php",
+  VISITAS: {
+    BY_UNIQUEID: "/visita/read-qr/index.php?qr={qr}",
+    VEHICLES: "/visita/getVisitVehicles/index.php?qr={qr}",
+    UPDATE: "/visita/update/index.php",
+  },
+  VIGILANTE: {
+    CODE: "/vigilante/activationCode/index.php",
+  },
 };
 
 export const ROUTES = {
-	ACTIVATION_CODE: "activation-code",
-	HOME: "Vigilante-qr",
-	VISIT_INFO: "VisitaInfo",
-	CAMERA: "Camera",
+  ACTIVATION_CODE: "activation-code",
+  HOME: "Vigilante-qr",
+  VISIT_INFO: "VisitaInfo",
+  CAMERA: "Camera",
 };
 
 const TIME_ZONES = {
-	CST: {
-		id: 6,
-		label: "Hora del centro",
-	},
+  CST: {
+    id: 6,
+    label: "Hora del centro",
+  },
 };
 
 export const getTimeZone = (offset: number) => {
-	const diffHrs = offset / 60;
-	const hours = Math.floor(diffHrs);
-	const tz = Object.values(TIME_ZONES).find((tz) => tz.id === hours);
-	return tz?.label;
+  const diffHrs = offset / 60;
+  const hours = Math.floor(diffHrs);
+  const tz = Object.values(TIME_ZONES).find((tz) => tz.id === hours);
+  return tz?.label;
 };
 
 export const timeFormat = (time: number) =>
-	time < 10 ? `0${time}` : time.toString();
+  time < 10 ? `0${time}` : time.toString();
 
 export const timeZone = "es-MX";
 export const dateFormat = "YYYY-MM-DD";
 
 export const getLabelApp = (lang: string, key: string) => {
-	let language = "";
-	switch (lang) {
-		case "es":
-			language = JSON.stringify(lang_es);
-			break;
-		default:
-			language = JSON.stringify(lang_es);
-			break;
-	}
-	const labels = JSON.parse(language);
-	return labels[key];
+  let language = "";
+  switch (lang) {
+    case "es":
+      language = JSON.stringify(lang_es);
+      break;
+    default:
+      language = JSON.stringify(lang_es);
+      break;
+  }
+  const labels = JSON.parse(language);
+  return labels[key];
 };
 
 export const militarToTwelveHours = (
-	hour: number
+  hour: number
 ): { hour: number; ampm: string } => {
-	const ampm = hour >= 12 ? "PM" : "AM";
-	if (hour > 12) {
-		return {
-			hour: hour - 12,
-			ampm,
-		};
-	}
-	return {
-		hour,
-		ampm,
-	};
+  const ampm = hour >= 12 ? "PM" : "AM";
+  if (hour > 12) {
+    return {
+      hour: hour - 12,
+      ampm,
+    };
+  }
+  return {
+    hour,
+    ampm,
+  };
 };
 
 export const toMilitarHours = (hour: number, ampm: string) => {
-	if (ampm === "PM") {
-		return hour + 12;
-	}
-	return hour;
+  if (ampm === "PM") {
+    return hour + 12;
+  }
+  return hour;
 };
