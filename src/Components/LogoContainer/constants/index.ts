feb1dbd2f5748b652d5773f74e8bd64b4e71cccb
MFuentes1992 <markfuentes2991@gmail.com>
2024-10-08 17:14:53 -0600

    Add Caseta logo to nav bard

---
+++ src/Components/LogoContainer/constants/index.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const LogoContainerStyles = StyleSheet.create({
+  container: {
+    position: "relative",
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 20,
+  },
+  logo: {
+    width: 30,
+    height: 30,
+    borderRadius: 50,
+  },
+});
